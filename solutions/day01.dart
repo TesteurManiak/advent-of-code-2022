2111040979439429b7ec4af927da36905bd3d3e8
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-01 10:52:26 +0100

    day 1

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,38 +1,50 @@
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+  List<List<int>> parseInput() {
+    final elfs = <List<int>>[];
+    final calories = <int>[];
+    final inputLines = input.getPerLine();
+    for (final line in inputLines) {
+      if (line.isEmpty) {
+        elfs.add(List<int>.from(calories));
+        calories.clear();
+      } else {
+        calories.add(int.parse(line));
+      }
+    }
+    return elfs;
   }
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    final results = parseInput();
+
+    int maxCalories = -1;
+    for (final elf in results) {
+      final calories = elf.reduce((value, e) => value + e);
+      if (calories > maxCalories) {
+        maxCalories = calories;
+      }
+    }
+    return maxCalories;
   }
 
   @override
   int solvePart2() {
-    // TODO implement
-    return 0;
+    final results = parseInput();
+    final topThreeMaxCalories = <int>[-1, -1, -1];
+
+    for (final elf in results) {
+      final calories = elf.reduce((value, e) => value + e);
+      topThreeMaxCalories.sort();
+      if (topThreeMaxCalories[0] < calories) {
+        topThreeMaxCalories[0] = calories;
+      }
+    }
+    return topThreeMaxCalories.reduce((value, e) => value + e);
   }
 }
