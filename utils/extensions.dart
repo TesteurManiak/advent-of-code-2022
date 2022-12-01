93ffdf113bf735d2456dba61beb3b81c45c0e6b2
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-01 17:04:06 +0100

    upgraded dependencies

--- utils/extensions.dart
+++ utils/extensions.dart
@@ -1,9 +1,4 @@
-import 'dart:math' as math;
-
 extension IterableExtension<T extends num> on Iterable<T> {
-  T get max => reduce(math.max);
-  T get min => reduce(math.min);
-
   Iterable<T> topMax(int length) {
     final list = toList()..sort((a, b) => b.compareTo(a));
     return list.take(length);
