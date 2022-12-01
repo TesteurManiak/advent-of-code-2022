047fd6370ff5d9b435f75e383eaad39cecb51de9
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-01 12:28:45 +0100

    improved algorithm

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,16 +1,18 @@
+import 'dart:math' as math;
+
 import '../utils/index.dart';
 
 class Day01 extends GenericDay {
   Day01() : super(1);
 
   @override
-  List<List<int>> parseInput() {
-    final elfs = <List<int>>[];
+  List<int> parseInput() {
+    final elfs = <int>[];
     final calories = <int>[];
     final inputLines = input.getPerLine();
     for (final line in inputLines) {
       if (line.isEmpty) {
-        elfs.add(List<int>.from(calories));
+        elfs.add(calories.reduce((a, b) => a + b));
         calories.clear();
       } else {
         calories.add(int.parse(line));
@@ -21,28 +23,24 @@ class Day01 extends GenericDay {
 
   @override
   int solvePart1() {
-    final results = parseInput();
+    final results = parsedInput;
 
     int maxCalories = -1;
     for (final elf in results) {
-      final calories = elf.reduce((value, e) => value + e);
-      if (calories > maxCalories) {
-        maxCalories = calories;
-      }
+      maxCalories = math.max(maxCalories, elf);
     }
     return maxCalories;
   }
 
   @override
   int solvePart2() {
-    final results = parseInput();
+    final results = parsedInput;
     final topThreeMaxCalories = <int>[-1, -1, -1];
 
     for (final elf in results) {
-      final calories = elf.reduce((value, e) => value + e);
       topThreeMaxCalories.sort();
-      if (topThreeMaxCalories[0] < calories) {
-        topThreeMaxCalories[0] = calories;
+      if (topThreeMaxCalories[0] < elf) {
+        topThreeMaxCalories[0] = elf;
       }
     }
     return topThreeMaxCalories.reduce((value, e) => value + e);
