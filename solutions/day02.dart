acee7efff55d12660f0c29fed6b306703da4eb95
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-02 08:52:13 +0100

    day 2

---
+++ solutions/day02.dart
@@ -0,0 +1,111 @@
+import '../utils/index.dart';
+
+class Day02 extends GenericDay {
+  Day02() : super(2);
+
+  @override
+  List<List<String>> parseInput() {
+    final inputLines = input.getPerLine();
+    final results = <List<String>>[];
+    for (final line in inputLines) {
+      final battle = line.split(' ');
+      results.add(battle);
+    }
+    return results;
+  }
+
+  @override
+  int solvePart1() {
+    final inputLines = parseInput();
+    int total = 0;
+    for (final line in inputLines) {
+      final opponent =
+          RPSValues.values.firstWhere((e) => e.opponentPlay == line[0]);
+      final you = RPSValues.values.firstWhere((e) => e.yourPlay == line[1]);
+
+      if (you.isWin(opponent)) {
+        total += scoreForAWin;
+      } else if (you.isDraw(opponent)) {
+        total += scoreForADraw;
+      }
+
+      total += you.score;
+    }
+    return total;
+  }
+
+  @override
+  int solvePart2() {
+    final inputLines = parseInput();
+    int total = 0;
+    for (final line in inputLines) {
+      final opponent =
+          RPSValues.values.firstWhere((e) => e.opponentPlay == line[0]);
+      final result = ResultValues.values.firstWhere((e) => e.letter == line[1]);
+
+      switch (result) {
+        case ResultValues.win:
+          total += scoreForAWin;
+          total += counterFor(opponent).score;
+          break;
+        case ResultValues.draw:
+          total += scoreForADraw;
+          total += opponent.score;
+          break;
+        case ResultValues.loss:
+          total += lossFor(opponent).score;
+          break;
+      }
+    }
+    return total;
+  }
+}
+
+const scoreForAWin = 6;
+const scoreForADraw = 3;
+
+enum RPSValues {
+  rock('A', 'X', 'Y', 1),
+  paper('B', 'Y', 'Z', 2),
+  scissors('C', 'Z', 'X', 3);
+
+  final String opponentPlay;
+  final String yourPlay;
+  final String counter;
+  final int score;
+
+  const RPSValues(
+    this.opponentPlay,
+    this.yourPlay,
+    this.counter,
+    this.score,
+  );
+}
+
+extension on RPSValues {
+  bool isWin(RPSValues other) {
+    return yourPlay == other.counter;
+  }
+
+  bool isDraw(RPSValues other) {
+    return yourPlay == other.yourPlay;
+  }
+}
+
+RPSValues counterFor(RPSValues value) {
+  return RPSValues.values.firstWhere((e) => e.yourPlay == value.counter);
+}
+
+RPSValues lossFor(RPSValues value) {
+  return RPSValues.values.firstWhere(
+      (e) => e.yourPlay != value.counter && e.yourPlay != value.yourPlay);
+}
+
+enum ResultValues {
+  win('Z'),
+  draw('Y'),
+  loss('X');
+
+  final String letter;
+  const ResultValues(this.letter);
+}
