1a8042b8860f492fcf8c2a8ade7f40a33e0f7d93
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-04 11:49:17 +0100

    day 4

---
+++ solutions/day04.dart
@@ -0,0 +1,77 @@
+import '../utils/index.dart';
+
+class Day04 extends GenericDay {
+  Day04() : super(4);
+
+  @override
+  List<Tuple2<Pair, Pair>> parseInput() {
+    final lineInputs = input.getPerLine();
+    final list = <Tuple2<Pair, Pair>>[];
+    for (final line in lineInputs) {
+      final splittedPairs = line.split(',');
+      final firstPair = Pair.fromString(splittedPairs.first);
+      final secondPair = Pair.fromString(splittedPairs.last);
+      list.add(Tuple2(firstPair, secondPair));
+    }
+    return list;
+  }
+
+  @override
+  int solvePart1() {
+    final pairs = parseInput();
+    int overlaps = 0;
+    for (final doublePairs in pairs) {
+      final firstPair = doublePairs.item1;
+      final secondPair = doublePairs.item2;
+      if (firstPair.isFullyOverlapping(secondPair)) {
+        overlaps++;
+      }
+    }
+    return overlaps;
+  }
+
+  @override
+  int solvePart2() {
+    final pairs = parseInput();
+    int overlaps = 0;
+    for (final doublePairs in pairs) {
+      final firstPair = doublePairs.item1;
+      final secondPair = doublePairs.item2;
+      if (firstPair.isPartiallyOverlapping(secondPair)) {
+        overlaps++;
+      }
+    }
+    return overlaps;
+  }
+}
+
+class Pair {
+  final int start;
+  final int end;
+
+  const Pair(this.start, this.end);
+
+  Pair.fromString(String str)
+      : this(
+          int.parse(str.split('-').first),
+          int.parse(str.split('-').last),
+        );
+
+  bool isFullyOverlapping(Pair other) {
+    if (start <= other.start && end >= other.end) {
+      return true;
+    } else if (other.start <= start && other.end >= end) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isPartiallyOverlapping(Pair other) {
+    if (start <= other.start && end >= other.start) {
+      return true;
+    } else if (other.start <= start && other.end >= start) {
+      return true;
+    }
+    return false;
+  }
+}
