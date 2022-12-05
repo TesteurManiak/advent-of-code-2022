4fbd109be26abaf8b7963b016698af4e1c751578
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-05 09:30:14 +0100

    day 5

---
+++ solutions/day05.dart
@@ -0,0 +1,136 @@
+import '../utils/index.dart';
+import '../utils/extensions.dart';
+
+class Day05 extends GenericDay {
+  Day05() : super(5);
+
+  @override
+  Tuple2<Map<int, List<String>>, List<Movement>> parseInput() {
+    const width = 4;
+
+    final stacks = <int, List<String>>{};
+    final moves = <Movement>[];
+    final inputLines = input.getPerLine();
+
+    int y = 0;
+    while (y < inputLines.length) {
+      final line = inputLines[y];
+
+      int column = 0;
+      for (int x = 0; x < line.length; x += width) {
+        final subStr = line.substring(x);
+        final str = String.fromCharCodes(subStr.codeUnits.take(width));
+
+        stacks.putIfAbsent(column, () => []);
+
+        if (!RegExp(r'\d').hasMatch(str)) {
+          final cleaned = str.replaceAll(RegExp(r'[\[\]]'), '').trim();
+          if (cleaned.isNotEmpty) {
+            stacks[column]?.add(cleaned);
+          }
+        }
+        column++;
+      }
+      y++;
+      if (line.isEmpty) break;
+    }
+
+    while (y < inputLines.length) {
+      final line = inputLines[y];
+      moves.add(Movement.fromString(line));
+      y++;
+    }
+
+    return Tuple2(reverseStacks(stacks), moves);
+  }
+
+  @override
+  String solvePart1() {
+    final parsed = parseInput();
+    Map<int, List<String>> stacks = parsed.item1;
+    final moves = parsed.item2;
+
+    for (final move in moves) {
+      stacks = moveStacksOneByOne(move, stacks);
+    }
+
+    return stacks.values.map((e) => e.last).join();
+  }
+
+  @override
+  String solvePart2() {
+    final parsed = parseInput();
+    Map<int, List<String>> stacks = parsed.item1;
+    final moves = parsed.item2;
+
+    for (final move in moves) {
+      stacks = moveStackesAllTogether(move, stacks);
+    }
+
+    return stacks.values.map((e) => e.last).join();
+  }
+}
+
+Map<int, List<String>> reverseStacks(Map<int, List<String>> stacks) {
+  final reversed = <int, List<String>>{};
+
+  for (final stack in stacks.entries) {
+    reversed.putIfAbsent(stack.key, () => []);
+    reversed[stack.key]?.addAll(stack.value.reversed);
+  }
+  return reversed;
+}
+
+Map<int, List<String>> moveStacksOneByOne(
+  Movement move,
+  Map<int, List<String>> stacks,
+) {
+  final newStacks = Map<int, List<String>>.from(stacks);
+  final startStack = newStacks[move.start]!;
+  final endStack = newStacks[move.end]!;
+
+  for (int i = 0; i < move.numberOfSteps; i++) {
+    final elem = startStack.lastOrNull;
+
+    if (elem == null) break;
+    startStack.removeLast();
+    endStack.add(elem);
+  }
+
+  return newStacks;
+}
+
+Map<int, List<String>> moveStackesAllTogether(
+  Movement move,
+  Map<int, List<String>> stacks,
+) {
+  final newStacks = Map<int, List<String>>.from(stacks);
+  final startStack = newStacks[move.start]!;
+  final endStack = newStacks[move.end]!;
+
+  final elems = startStack.takeLast(move.numberOfSteps).toList();
+  startStack.removeRange(
+      startStack.length - move.numberOfSteps, startStack.length);
+  endStack.addAll(elems);
+
+  return newStacks;
+}
+
+class Movement {
+  final int numberOfSteps;
+  final int start;
+  final int end;
+
+  const Movement(this.numberOfSteps, this.start, this.end);
+
+  factory Movement.fromString(String input) {
+    final parts = input.split(' ')
+      ..removeWhere((element) => !RegExp(r'\d').hasMatch(element));
+
+    final numberOfSteps = int.parse(parts[0]);
+    final start = int.parse(parts[1]) - 1;
+    final end = int.parse(parts[2]) - 1;
+
+    return Movement(numberOfSteps, start, end);
+  }
+}
--- utils/extensions.dart
+++ utils/extensions.dart
@@ -1,4 +1,4 @@
-extension IterableExtension<T extends num> on Iterable<T> {
+extension NumIterableExtension<T extends num> on Iterable<T> {
   Iterable<T> topMax(int length) {
     final list = toList()..sort((a, b) => b.compareTo(a));
     return list.take(length);
@@ -7,5 +7,12 @@ extension IterableExtension<T extends num> on Iterable<T> {
   Iterable<T> topMin(int length) {
     final list = toList()..sort();
     return list.take(length);
+  }
+}
+
+extension IterableExtensions<T> on Iterable<T> {
+  Iterable<T> takeLast(int count) {
+    final list = toList();
+    return list.skip(list.length - count);
   }
 }
