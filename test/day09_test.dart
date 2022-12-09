4f9bfe2fbb935e432fe3a2c539d7dfee59df46e1
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-09 13:46:41 +0100

    improved testing

--- test/day09_test.dart
+++ test/day09_test.dart
@@ -4,28 +4,33 @@ import '../solutions/day09.dart';
 
 void main() {
   group('Day09', () {
-    final day = Day09();
+    late Day09 day;
 
-    test(
-      'part1',
-      () {
-        final inputLines = <String>[
-          'R 4',
-          'U 4',
-          'L 3',
-          'D 1',
-          'R 4',
-          'D 1',
-          'L 5',
-          'R 2',
-        ];
-        day.input.inputAsList = inputLines;
+    setUp(() {
+      day = Day09();
+    });
+
+    group('part1', () {
+      final inputLines = <String>[
+        'R 4',
+        'U 4',
+        'L 3',
+        'D 1',
+        'R 4',
+        'D 1',
+        'L 5',
+        'R 2',
+      ];
+
+      setUp(() => day.input.inputAsList = inputLines);
+
+      test('solvePart1', () {
         final result = day.solvePart1();
         expect(result, 13);
-      },
-    );
+      });
+    });
 
-    test('part2', () {
+    group('part2', () {
       final inputLines = <String>[
         'R 5',
         'U 8',
@@ -36,9 +41,13 @@ void main() {
         'L 25',
         'U 20',
       ];
-      day.input.inputAsList = inputLines;
-      final result = day.solvePart2();
-      expect(result, 36);
+
+      setUp(() => day.input.inputAsList = inputLines);
+
+      test('part2', () {
+        final result = day.solvePart2();
+        expect(result, 36);
+      });
     });
   });
 }
