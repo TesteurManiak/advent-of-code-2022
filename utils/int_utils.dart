c852fe494c30486a0671f9be334fa12fbb0dbf5a
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-11 11:07:08 +0100

    simplified code

--- utils/int_utils.dart
+++ utils/int_utils.dart
@@ -1,16 +1,2 @@
-/// Return the greatest common divisor of two integers.
-int gcd(int a, int b) {
-  int localA = a;
-  int localB = b;
-  while (localB != 0) {
-    final temp = localB;
-    localB = localA % localB;
-    localA = temp;
-  }
-  return localA;
-}
-
 /// Return the lowest common multiple of two integers.
-int lcm(int a, int b) {
-  return (a * b) ~/ gcd(a, b);
-}
+int lcm(int a, int b) => (a * b) ~/ a.gcd(b);
