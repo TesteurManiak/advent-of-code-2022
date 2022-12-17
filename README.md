8a7121122c40926a2507be2fc78aa5b3fafc56e0
Guillaume Roux <rouxguillaume8@gmail.com>
2022-12-17 11:37:33 +0100

    base

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 You can check my utility methods in the [extensions.dart](utils/extensions.dart) file.
 
+Stopped at day 17 part 2.
+
 <a href="https://adventofcode.com/2022">
     <img src="https://blogs.sap.com/wp-content/uploads/2020/11/EkaoQQTXEAMA4BN.jpg">
 </a>
--- solutions/day17.dart
+++ solutions/day17.dart
@@ -4,19 +4,59 @@ class Day17 extends GenericDay {
   Day17() : super(17);
 
   @override
-  parseInput() {
-    
+  List<Position> parseInput() {
+    const moves = [Position(-1, 0), Position(1, 0)];
+    const patterns = '<>';
+    return input
+        .getPerLine()
+        .first
+        .split('')
+        .map((e) => moves[patterns.indexOf(e)])
+        .toList();
   }
 
   @override
-  solvePart1() {
-    
+  int solvePart1() {
+    final jetPattern = parseInput();
     return 0;
   }
 
   @override
-  solvePart2() {
-
+  int solvePart2() {
+    final jetPattern = parseInput();
     return 0;
   }
+}
+
+enum Tetrimino {
+  horizontalBar(
+    [Position(0, 0), Position(1, 0), Position(2, 0), Position(3, 0)],
+    4,
+  ),
+  plus(
+    [Position(1, 0), Position(0, 1), Position(2, 1), Position(1, 2)],
+    3,
+  ),
+  mirrorL(
+    [
+      Position(0, 0),
+      Position(1, 0),
+      Position(2, 0),
+      Position(2, 1),
+      Position(2, 2)
+    ],
+    3,
+  ),
+  verticalBar(
+    [Position(0, 0), Position(0, 1), Position(0, 2), Position(0, 3)],
+    1,
+  ),
+  square(
+    [Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1)],
+    2,
+  );
+
+  const Tetrimino(this.shape, this.width);
+  final List<Position> shape;
+  final int width;
 }
